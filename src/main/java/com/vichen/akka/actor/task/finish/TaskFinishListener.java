8b98aa2ade7b9ed205688e8708e7ad9f2fc30830
yu.chen <yu.chen@fairlinkcentury.com>
2019-09-12 10:38:43 +0800

    根任务重算不能影响taskIndex

--- src/main/java/com/vichen/akka/actor/task/finish/TaskFinishListener.java
+++ src/main/java/com/vichen/akka/actor/task/finish/TaskFinishListener.java
@@ -3,6 +3,7 @@ package com.vichen.akka.actor.task.finish;
 import akka.actor.AbstractActor;
 import com.alibaba.fastjson.JSON;
 import com.vichen.SpringContextUtil;
+import com.vichen.akka.actor.task.ReCalculate;
 import com.vichen.akka.actor.task.index.TaskIndex;
 import com.vichen.akka.actor.task.index.TaskIndexRepository;
 import org.slf4j.Logger;
@@ -28,7 +29,8 @@ public class TaskFinishListener extends AbstractActor {
       logger.info("TaskFinishListener result:{}", JSON.toJSONString(taskFinishData));
       taskFinishRepository.save(taskFinishData);
 
-      if (taskFinishData.isRootTask()) {
+      //根任务+不是重算
+      if (taskFinishData.isRootTask() && !(taskFinishData.getCalculate() instanceof ReCalculate)) {
         taskIndexRepository.save(
           new TaskIndex(taskFinishData.getTaskName(), taskFinishData.getPlaneUniqueNo(),
             taskFinishData.getTaskId(), taskFinishData.getCalculate().getEnd()));
